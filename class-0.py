e32ad477f3de5d26564c12c3024254935b507fc0
Habibul Islam <habibulislam6862@gmail.com>
2024-01-03 12:44:44 +0600

    Class 1 is incomplete

---
+++ class-0.py
@@ -0,0 +1,45 @@
+'''
+Data type
+# string -> quatation is mandatory
+# number | integer
+# float
+# boolean -> True | False
+'''
+
+
+'''
+what is variable?
+# variable - vary + able
+# storage
+'''
+
+# name = "Imran"
+# print(   "This is " +  name     )
+
+'''
+concat korte hole + diye korte hoy
+concat kora = zora lagano
+'''
+
+# x = 10
+# y = 20
+# print(x + y)
+
+'''
+# + is a overloaded operator becase we can do sum and concat with + sign
+'''
+
+
+# x = 22
+# y = 5
+
+# print(x % y)
+
+# x = 22
+# x -= 3 
+# x = x - 3
+# print(x)
+
+'''
+next class will be started from comparison operator
+'''
